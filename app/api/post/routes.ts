1d85a1c7f215bb8f48d0026965e0f3fce7464bd6
DejanBancevic <alies22222@gmail.com>
2025-06-11 12:10:18 +0200

    google auth test 2

--- app/api/post/routes.ts
+++ app/api/post/routes.ts
@@ -2,24 +2,7 @@ import prisma from '../../../lib/prisma';
 import { NextResponse } from "next/server";
 import { getSession } from "../../actions/getCurrentUser";
 
-export async function POST(request: { json: () => any; }) {
-    const body = await request.json()
-    const { name, recipe, ingredients, date } = body;
-    const session = await getSession();
-    const result = await prisma.health.create({
-        data: {
-            name: name,
-            recipe: recipe,
-            ingredients: ingredients,
-            date: date,
-            author: { connect: { email: session?.user?.email } },
-        },
-    });
 
-    return NextResponse.json({
-        data: body
-    });
-}
 
 export async function DELETE(request: { json: () => any; }) {
     const body = await request.json()
